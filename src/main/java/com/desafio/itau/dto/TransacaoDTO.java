086b9bdb5203ab1aee111e2b16ffcc724e26e15c
Matheus de Sousa Almeida <matheusalmeidass14@gmail.com>
2025-03-18 18:43:09 -0300

    feat: restrictions applied

--- src/main/java/com/desafio/itau/dto/TransacaoDTO.java
+++ src/main/java/com/desafio/itau/dto/TransacaoDTO.java
@@ -1,4 +1,27 @@
 package com.desafio.itau.dto;
 
+import java.time.OffsetDateTime;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public class TransacaoDTO {
+    @NotNull @PositiveOrZero
+    private double valor;
+
+    @NotNull @Past
+    private OffsetDateTime dataHora;
+
+    public TransacaoDTO(double valor, OffsetDateTime dataHora) {
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
 }
